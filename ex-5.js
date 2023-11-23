4a3928464d10653ad14853106dcda22d80ac28db
datfoi <jengjiening@gmail.com>
2023-11-23 14:21:27 +0700

    feat: assignment-1-8/fai

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let fullPrice = 27000;
+let percentDiscout25 = (25 / 100) * fullPrice;
+let totalPrice = fullPrice - percentDiscout25;
+
+console.log(totalPrice);
